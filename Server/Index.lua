efa85e1b2b202d6e36b3720086b84fffaad72312
Gabriel T. Nardy <gtnardy@gmail.com>
2020-11-10 06:24:27 -0300

    Tweaked some vehicles

--- Server/Index.lua
+++ Server/Index.lua
@@ -27,8 +27,8 @@ NanosWorldVehicles = {
 
 		vehicle:SetWheel(0, "Wheel_Front_Left",  26, 17, 50)
 		vehicle:SetWheel(1, "Wheel_Front_Right", 26, 17, 50)
-		vehicle:SetWheel(2, "Wheel_Rear_Left",   26, 17,  0, 20, 0.25, 2, 17, 1000, 30, 20, 10, 10)
-		vehicle:SetWheel(3, "Wheel_Rear_Right",  26, 17,  0, 20, 0.25, 2, 17, 1000, 30, 20, 10, 10)
+		vehicle:SetWheel(2, "Wheel_Rear_Left",   26, 17,  0, 20, 0.25, 2, 17, 1000, 50, 20, 10, 10)
+		vehicle:SetWheel(3, "Wheel_Rear_Right",  26, 17,  0, 20, 0.25, 2, 17, 1000, 50, 20, 10, 10)
 
 		vehicle:SetDoor(0, Vector(25, -80, 100), Vector( 0, -47, 80), Rotator(0, 0, 10), 60, -150)
 		vehicle:SetDoor(1, Vector(25,  80, 100), Vector(12,  47, 80), Rotator(0, 0,  0), 60,  150)
@@ -40,7 +40,7 @@ NanosWorldVehicles = {
 		local vehicle = Vehicle(location or Vector(), rotation or Rotator(), "NanosWorld::SK_Pickup", 0, true, false, true, "NanosWorld::A_Vehicle_Engine_10")
 		vehicle:SetEngineSetup(5000)
 		vehicle:SetSteeringWheelSetup(Vector(0, 27, 120), 24)
-		vehicle:SetHeadlightsSetup(Vector(250, 0, 70))
+		vehicle:SetHeadlightsSetup(Vector(270, 0, 70))
 
 		vehicle:SetWheel(0, "Wheel_Front_Left",  27, 18, 45, 20, 0.25, 2, 17, 1000, 50, 50, 20)
 		vehicle:SetWheel(1, "Wheel_Front_Right", 27, 18, 45, 20, 0.25, 2, 17, 1000, 50, 50, 20)
@@ -62,15 +62,15 @@ NanosWorldVehicles = {
 
 		vehicle:SetEngineSetup(7500, 0.25, 0.1)
 		vehicle:SetTransmissionSetup(true, 0.1, 1, 4, 20)
-		vehicle:SetSteeringWheelSetup(Vector(0, 31, 115), 17)
+		vehicle:SetSteeringWheelSetup(Vector(0, 38, 115), 17)
 
 		vehicle:SetWheel(0, "Wheel_Front_Left",  30, 27, 50)
 		vehicle:SetWheel(1, "Wheel_Front_Right", 30, 27, 50)
 		vehicle:SetWheel(2, "Wheel_Rear_Left",   36, 37,  0, 20, 0.25, 2, 17, 1000, 0, 10, 10, 7)
 		vehicle:SetWheel(3, "Wheel_Rear_Right",  36, 37,  0, 20, 0.25, 2, 17, 1000, 0, 10, 10, 7)
 
-		vehicle:SetDoor(0, Vector(25, -95, 100), Vector(22, -42, 62), Rotator(0, 0,  0), 75, -150)
-		vehicle:SetDoor(1, Vector(25,  95, 100), Vector(18,  42, 64), Rotator(0, 0, -5), 75,  150)
+		vehicle:SetDoor(0, Vector(25, -95, 100), Vector(35, -42, 55), Rotator(0, 0, -10), 75, -150)
+		vehicle:SetDoor(1, Vector(25,  95, 100), Vector(35,  42, 60), Rotator(0, 0, -15), 75,  150)
 
 		vehicle:RecreatePhysics()
 		return vehicle
@@ -102,8 +102,8 @@ NanosWorldVehicles = {
 
 		vehicle:SetWheel(0, "Wheel_Front_Left",  43, 25, 40,  20, 0.25, 2, 17, 1000, 75, 30, 10)
 		vehicle:SetWheel(1, "Wheel_Front_Right", 43, 25, 40,  20, 0.25, 2, 17, 1000, 75, 30, 10)
-		vehicle:SetWheel(2, "Wheel_Rear_Left",   43, 50,  0, 100, 0.25, 2, 17, 1000, 75, 30, 10)
-		vehicle:SetWheel(3, "Wheel_Rear_Right",  43, 50,  0, 100, 0.25, 2, 17, 1000, 75, 30, 10)
+		vehicle:SetWheel(2, "Wheel_Rear_Left",   43, 50,  0, 300, 0.25, 2, 17, 1000, 75, 30, 10)
+		vehicle:SetWheel(3, "Wheel_Rear_Right",  43, 50,  0, 300, 0.25, 2, 17, 1000, 75, 30, 10)
 
 		vehicle:SetDoor(0, Vector(235, -100, 132), Vector(210, -56, 150), Rotator(0, 0, 20), 100, -150)
 		vehicle:SetDoor(1, Vector(235,  100, 132), Vector(225,  60, 147), Rotator(0, 0, 10), 100,  150)
